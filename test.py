a19b596d6cde9ab52acf3576f7d89792c189c622
duyhuwng2910 <duyhungnguyen2002@gmail.com>
2024-02-21 19:43:34 +0700

    Updating and Testing real time data

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import json
 
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                         value_serializer=lambda x: json.dumps(x).encode('utf8'))
+producer = KafkaProducer(bootstrap_servers='localhost:9094',
+                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
 trade_list = []
 
--- test1.py
+++ test1.py
@@ -2,9 +2,14 @@ from kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer('demo',
-                         bootstrap_servers='localhost:9092',
+                         bootstrap_servers='localhost:9094',
                          auto_offset_reset='latest',
-                         value_deserializer=lambda x: json.loads(x.decode('utf8')))
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+llist = []
 
 for e in consumer:
     print(e.value)
+    llist.append(e.value)
+
+print(len(llist))
