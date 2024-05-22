4826aa8bc19016afa0ddc2cea77de935f04ad611
Nguyen Duy Hung <duyhungnguyen2002@gmail.com>
2024-05-22 17:05:51 +0700

    Commit date 22-05-2024: - Update Kafka docker-compose-non-dev.yaml

--- Kafka/init-kafka.sh
+++ Kafka/init-kafka.sh
@@ -4,10 +4,10 @@ docker exec -it kafka-controller-1 bash
 
 ## Tạo kafka topic
 # Ở đây, tạo một topic với số replication factor là 1 và partitions là 2
-kafka-topics --create --topic stock --bootstrap-server kafka-controller-1:9092 --replication-factor 1 --partitions 2
+kafka-topics --create --topic stock --bootstrap-server kafka-controller-1:9092 --replication-factor 2 --partitions 3
 
 # Nếu khởi chạy cụm Kafka on production thì chạy dòng sau
-kafka-topics --create --topic stock --bootstrap-server kafka-controller-1:9092 --replication-factor 2 --partitions 3
+kafka-topics --create --topic stock --bootstrap-server kafka-controller-1:9092 --replication-factor 3 --partitions 4
 
 # Kiểm tra danh sách các topic có trong hệ thống
 kafka-topics --list --bootstrap-server kafka-controller-1:9092
@@ -17,9 +17,3 @@ kafka-topics --describe --topic stock --bootstrap-server kafka-controller-1:9092
 
 # Kiểm tra bản tin được gửi lên topic
 kafka-console-consumer --topic stock --bootstrap-server kafka-broker-1:9093
-
-kafka-topics --create --topic hose --bootstrap-server kafka-controller-1:9092 --replication-factor 1 --partitions 2
-
-kafka-topics --create --topic hnx --bootstrap-server kafka-controller-1:9092 --replication-factor 1 --partitions 2
-
-kafka-topics --create --topic upcom --bootstrap-server kafka-controller-1:9092 --replication-factor 1 --partitions 2
